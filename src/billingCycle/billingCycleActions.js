696ea3a1ef737a923c555f6f802c22cf95558996
Giuliano Bortoloni <giulianoagria@hotmail.com>
2019-07-05 20:56:01 -0300

    terminando o create no form

--- src/billingCycle/billingCycleActions.js
+++ src/billingCycle/billingCycleActions.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 import { toastr } from "react-redux-toastr";
+import { reset as resetForm } from "redux-form";
+
+import { selectTab, showTabs } from "../common/tab/tabActions";
 
 const BASE_URL = "http://localhost:3003/api";
 
@@ -12,15 +15,23 @@ export function getList() {
 }
 
 export function create(values) {
-  axios
-    .post(`${BASE_URL}/billingCycles`, values)
-    .then(resp => {
-      toastr.success("Sucesso", "Operação realizada com sucesso.");
-    })
-    .catch(e => {
-      toastr.error("Erro", "Houve um problema");
-    });
-  return {
-    type: "TEMP"
+  return dispatch => {
+    axios
+      .post(`${BASE_URL}/billingCycles`, values)
+      .then(resp => {
+        toastr.success("Sucesso", "Operação realizada com sucesso.");
+        dispatch([
+          resetForm("billingCycleForm"),
+          getList(),
+          selectTab("tabList"),
+          showTabs("tabList", "tabCreate")
+        ]);
+      })
+      .catch(e => {
+        toastr.error("Erro", "Houve um problema");
+      });
+    return {
+      type: "TEMP"
+    };
   };
 }
